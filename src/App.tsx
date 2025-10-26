dc1fd0f4e7dfe82bd824eb6a6b945fc457c01738
Teddee Jordan <teddeejordan@192.168.1.7>
2025-10-26 15:12:20 +0700

    scaffolding app and adding axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import Search from './components/Search/Search'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): React.JSX.Element {
 
+  const params = { api_key: import.meta.env.VITE_TBMD_API_KEY }
+  const headers = {'Authorization': import.meta.env.VITE_TMMD_READ_ACCESS_TOKEN}
+  
+  const axiosGet = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}movie/${searchTerm}`, { params: params, headers: headers })
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const [searchTerm, setSearchTerm] = useState("")
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <img src='./hero-img.png' alt='Hero Banner' />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
