5aa0a5b37707adbff9f9d47715e888ca717c50ce
Teddee Jordan <teddeejordan@192.168.1.7>
2025-10-26 16:10:11 +0700

    finished scaffolding api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import React, { useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search/Search'
 import axios from 'axios'
+import { MovieTile, type IMovieData } from './components/MovieTile/MovieTile'
 
 function App(): React.JSX.Element {
-
+  const [errorMessage, setErrorMessage] = useState('')
+  const [popularMovies, setPopulsarMovies] = useState<IMovieData[] | undefined>(undefined)
+const [isLoading, setIsLoading] = useState(true)
+  
   const params = { api_key: import.meta.env.VITE_TBMD_API_KEY }
-  const headers = {'Authorization': import.meta.env.VITE_TMMD_READ_ACCESS_TOKEN}
+  const headers = {'Authorization': `Bearer ${import.meta.env.VITE_TMMD_READ_ACCESS_TOKEN}`}
   
   const axiosGet = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}movie/${searchTerm}`, { params: params, headers: headers })
+      const response = await axios.get(`${import.meta.env.VITE_BASE_URL}discover/movie?sort_by=popularity.desc`, { params: params, headers: headers })
+      setPopulsarMovies(response.data.results)
       console.log(response.data)
     } catch (error) {
-      console.log(error)
+      console.error(error)
+      setErrorMessage("Error fetching movies. Try again later")
+    } finally {
+      setIsLoading(false)
     }
   }
+
+  useEffect(() => {
+    axiosGet()
+  },[])
 
   const [searchTerm, setSearchTerm] = useState("")
   return (
@@ -25,8 +37,17 @@ function App(): React.JSX.Element {
         <header>
           <img src='./hero-img.png' alt='Hero Banner' />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
-        </header>
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className='all-moviles'>
+          <h2>All Movies</h2>
+          {isLoading ? <p>Is Loading</p> : popularMovies?.map((item: IMovieData) => {
+            return <MovieTile title={item.title} description={item.description} poster={item.poster} vote={item.vote} />
+          })}
+          {errorMessage && <p className='text-red-500'>
+            {errorMessage}
+          </p>}
+        </section>
       </div>
     </main>
   )
